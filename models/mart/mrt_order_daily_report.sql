51c9f3471403835390f1d74eecb053c3792794f5
Louis Dreyfus <louis.dreyfus@groupm.com>
2025-04-14 22:37:45 +0200

    commit number three

--- models/mart/mrt_order_daily_report.sql
+++ models/mart/mrt_order_daily_report.sql
@@ -1,5 +1,5 @@
 SELECT DATE_TRUNC(order_created_at, DAY) AS reporting_date,
-    3 AS column3,
+    3 AS column3333,
     mapping.account_manager,
     mapping.state,
     COUNT(DISTINCT order_id) AS total_orders,
